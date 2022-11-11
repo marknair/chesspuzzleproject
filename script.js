ee574d519610ead947ef4dad0c91a629d548d9a0
Mark Nair <marknair@me.com>
2022-11-11 16:25:31 -0600

    Initial JavaScript setup

---
+++ script.js
@@ -0,0 +1,9 @@
+let chessPieces = ["♟︎", "♘", "♟︎", "♘", "♟︎", "♝", "♟︎", "♝", "♜", "♟︎", "♟︎", "♜", "♕", "♔", "♟︎", "♟︎"]
+let pawnShelf = document.getElementById("pawn-shelf")
+let minorPiecesShelf = document.getElementById("minorpieces-shelf")
+let rookShelf = document.getElementById("rook-shelf")
+let royalShelf = document.getElementById("royal-shelf")
+
+function sortPieces() {
+  
+}
