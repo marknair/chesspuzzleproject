847f4b16a930244b2d460effb51901136d129bf8
Mark Nair <marknair@me.com>
2022-11-14 09:53:31 -0600

    Full js code for sorting

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-let chessPieces = ["♟︎", "♘", "♟︎", "♘", "♟︎", "♝", "♟︎", "♝", "♜", "♟︎", "♟︎", "♜", "♕", "♔", "♟︎", "♟︎", "Spanky"]
+let chessPieces = ["♟︎", "♘", "♟︎", "♘", "♟︎", "♝", "♟︎", "♝", "♜", "♟︎", "♟︎", "♜", "♕", "♔", "♟︎", "♟︎"]
 let pawnShelf = document.getElementById("pawn-shelf")
 let minorPiecesShelf = document.getElementById("minorpieces-shelf")
 let rookShelf = document.getElementById("rook-shelf")
 let royalShelf = document.getElementById("royal-shelf")
 
+//  Write a function that will sort through the array and put the pieces on different 'shelves' in the DOM.
+// 
+//  You should use a for-loop, a conditional statement, and the textContent property.
+// 
+//  1. Pawns go on the pawn shelf.
+//  2. Knights and bishops go on the minor pieces shelf.
+//  3. Rooks go on the rook shelf.
+//  4. The queen and king go on the royal shelf. 
+ 
+ 
 function sortPieces() {
-    // This is the work I'm about do.
+    for (let i = 0; i < chessPieces.length; i++) {
+        if (chessPieces[i] === "♟︎") {
+            pawnShelf.textContent += "♟︎"
+        }  else if (chessPieces[i] === "♘") {
+            minorPiecesShelf.textContent += "♘"
+        } else if (chessPieces[i] === "♝") {
+            minorPiecesShelf.textContent += "♝"
+        } else if (chessPieces[i] === "♜") {
+            rookShelf.textContent +=  "♜"
+        } else if (chessPieces[i] === "♕") {
+            royalShelf.textContent += "♕"
+        } else if (chessPieces[i] === "♔") {
+            royalShelf.textContent += "♔"
+        }
+    }
 }
 
 sortPieces()
